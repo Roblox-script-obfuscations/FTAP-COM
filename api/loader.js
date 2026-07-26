02238bcaefa18dbf987189ab6cf08376bea5eade
from-up <sdoy5357@gmail.com>
2026-07-26 17:22:23 +0900

    Fix: browser redirect, executor loadstring

--- api/loader.js
+++ api/loader.js
@@ -1,4 +1,9 @@
 export default function handler(req, res) {
+  const accept = req.headers['accept'] || '';
+  if (accept.includes('text/html')) {
+    res.setHeader('Location', '/');
+    return res.status(302).end();
+  }
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'text/plain');
   res.status(200).send('loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-script-obfuscations/ftap/refs/heads/main/dontlook.lua"))()');
