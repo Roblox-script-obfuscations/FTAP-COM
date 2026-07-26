fe190dd160e44883c40cbdff5cb1bc754f81a138
from-up <sdoy5357@gmail.com>
2026-07-26 17:18:44 +0900

    Fix: remove UA check

--- api/loader.js
+++ api/loader.js
@@ -1,12 +1,4 @@
 export default function handler(req, res) {
-  const ua = req.headers['user-agent'] || '';
-  const isRoblox = ua.toLowerCase().includes('roblox');
-
-  if (!isRoblox) {
-    res.setHeader('Location', '/v2/loader');
-    return res.status(302).end();
-  }
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'text/plain');
   res.status(200).send('loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-script-obfuscations/ftap/refs/heads/main/dontlook.lua"))()');
