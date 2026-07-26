742474ba3ae51fe12e5894d8fd7a578a1cf809d9
from-up <sdoy5357@gmail.com>
2026-07-26 17:15:22 +0900

    Hide code from browsers, allow only Roblox UA

--- api/loader.js
+++ api/loader.js
@@ -1,6 +1,13 @@
 export default function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send('loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-script-obfuscations/ftap/refs/heads/main/dontlook.lua"))()');
-    }
-    
+  const ua = req.headers['user-agent'] || '';
+  const isRoblox = ua.toLowerCase().includes('roblox');
+
+  if (!isRoblox) {
+    res.setHeader('Location', '/v2/loader');
+    return res.status(302).end();
+  }
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200).send('loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-script-obfuscations/ftap/refs/heads/main/dontlook.lua"))()');
+}
